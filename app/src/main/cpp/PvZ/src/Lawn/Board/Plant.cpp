a9028e4546b41f79fe9d118a91ceb97c915992b3
GarlicImp <2863663269@qq.com>
2026-09-22 19:03:08 +0800

    fix: 修复冰冻生菜在未触发时无敌

--- app/src/main/cpp/PvZ/src/Lawn/Board/Plant.cpp
+++ app/src/main/cpp/PvZ/src/Lawn/Board/Plant.cpp
@@ -3283,8 +3283,12 @@ bool Plant::IsDisposable(SeedType theSeedType) {
 }
 
 bool Plant::IsInvulnerable() {
+    if (mSeedType == SeedType::SEED_ICEBERG_LETTUCE) {
+        return mState == PlantState::STATE_READY;
+    }
+
     if (mSeedType == SeedType::SEED_CHERRYBOMB || mSeedType == SeedType::SEED_ICESHROOM || mSeedType == SeedType::SEED_DOOMSHROOM || mSeedType == SeedType::SEED_JALAPENO
-        || mSeedType == SeedType::SEED_BLOVER || mSeedType == SeedType::SEED_ICEBERG_LETTUCE || mSeedType == SeedType::SEED_CHILLY_PEPPER || mState == PlantState::STATE_SQUASH_LOOK
+        || mSeedType == SeedType::SEED_BLOVER || mSeedType == SeedType::SEED_CHILLY_PEPPER || mState == PlantState::STATE_SQUASH_LOOK
         || mState == PlantState::STATE_SQUASH_PRE_LAUNCH) {
         if (!mIsAsleep) {
             return true;
